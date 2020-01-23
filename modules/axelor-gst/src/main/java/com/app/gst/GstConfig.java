a901589f38b5fc04b4bc484ea8a58b605fbbe996
Yogi-Kaklottar <kaklottary@gmail.com>
2020-01-23 10:11:46 +0530

    to changes Address and config

--- modules/axelor-gst/src/main/java/com/app/gst/GstConfig.java
+++ modules/axelor-gst/src/main/java/com/app/gst/GstConfig.java
@@ -1,5 +1,7 @@
 package com.app.gst;
 
+import com.app.service.ServiceData;
+import com.app.service.ServiceImpl;
 import com.axelor.app.AxelorModule;
 import com.axelor.gst.db.repo.SequenceRepository;
 
@@ -7,6 +9,7 @@ public class GstConfig extends AxelorModule {
   @Override
   protected void configure() {
     bind(SequenceRepository.class).to(Sequencedata.class);
+    bind(ServiceData.class).to(ServiceImpl.class);
     super.configure();
   }
 }
